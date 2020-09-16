33882824729587c7427b9f6969ce3a017ac04cf6
Rsysz <ymps9640508@gmail.com>
2020-09-16 20:03:44 +0800

    Test1 q2 finished

--- test1.c
+++ test1.c
@@ -1,25 +1,28 @@
-//test
 
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
+
 bool is_ascii(const char str[], size_t size)
 {
     if (size == 0)
         return false;
     int i = 0;
-    while ((i + 8) <= size) {
-        uint64_t payload;
-        memcpy(&payload, str + i, 8);
-        if (payload & MMM)
-            return false;
-        i += 8;
-    }
     while (i < size) {
-        if (str[i] & 0x80)
-            return false;
-        i++;
+      if ((str[i] & 0x5F) < 65 || (str[i] & 0x5F) > 90)
+        return false;
+      i++;
     }
     return true;
+}
+
+void main() {
+  char t[] = {"sdsdsdsdrhjk"};
+  char s[] = {"Abds@djilajdl~!]"};
+  if (is_ascii(t, strlen(t)))
+    printf("TRUE\n");
+  if (is_ascii(s, strlen(s)))
+    printf("TRUE\n");
 }
